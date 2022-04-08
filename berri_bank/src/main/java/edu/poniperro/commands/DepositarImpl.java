a56d48876ce828ea1d8ac6db402fd600ed91f0f3
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-08 15:47:04 +0200

    feat: crear comando depositar

---
+++ berri_bank/src/main/java/edu/poniperro/commands/DepositarImpl.java
@@ -0,0 +1,28 @@
+package edu.poniperro.commands;
+
+import edu.poniperro.commands.intefaces.Order;
+
+public class DepositarImpl implements Order {
+
+    private Cuenta cuenta;
+    private double berri;
+
+    public DepositarImpl(Cuenta cuenta, double berri) {
+        this.cuenta = cuenta;
+        this.berri = berri;
+    }
+
+    private Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    private double getBerri() {
+        return berri;
+    }
+
+    @Override
+    public void execute() {
+        getCuenta().depositar(getBerri());
+    }
+
+}
