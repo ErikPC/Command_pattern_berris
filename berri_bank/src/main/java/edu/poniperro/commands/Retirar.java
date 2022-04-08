581e386e672381826ad2e2aacae1759ddae8d666
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-08 15:47:51 +0200

    feat: crear comando retirar

---
+++ berri_bank/src/main/java/edu/poniperro/commands/Retirar.java
@@ -0,0 +1,27 @@
+package edu.poniperro.commands;
+
+import edu.poniperro.commands.intefaces.Order;
+
+public class Retirar implements Order {
+    private Cuenta cuenta;
+    private double berri;
+
+    public Retirar(Cuenta cuenta, double berri) {
+        this.cuenta = cuenta;
+        this.berri = berri;
+    }
+
+    private double getBerri() {
+        return berri;
+    }
+
+    private Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    @Override
+    public void execute() {
+        getCuenta().retirar(getBerri());
+    }
+
+}
