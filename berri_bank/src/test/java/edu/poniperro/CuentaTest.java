87f6a62c02854ccb280f1a71e6cf7a79d42be65a
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-09 13:08:29 +0200

    test: crear test Cuenta

--- berri_bank/src/test/java/edu/poniperro/CuentaTest.java
+++ berri_bank/src/test/java/edu/poniperro/CuentaTest.java
@@ -1,26 +1,28 @@
 package edu.poniperro;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import edu.poniperro.commands.Cuenta;
 
 public class CuentaTest {
 
     Cuenta luffy = new Cuenta(1, 500);
-    Cuenta zoro = new Cuenta(2, 300);
     Cuenta usopp = new Cuenta(3, 200);
 
+    @DisplayName("Test Cuenta retirar()")
     @Test
-    public void testRetirarDinero() {
-        luffy.depositar(5.25);
-        assertEquals(luffy.getBerris(), 505.25, 0);
+    void testRetirar() {
+        luffy.retirar(250);
+        assertEquals(luffy.getBerris(), 250);
     }
 
+    @DisplayName("Test Cuenta depositar()")
     @Test
-    public void testDepositardinero() {
-        zoro.depositar(200);
-        assertEquals(zoro.getBerris(), 500, 0);
+    void testDepositar() {
+        usopp.depositar(2000);
+        assertEquals(usopp.getBerris(), 2200);
     }
 }
