71c1faf8b0a30426ec7868fe9eafea33464e5ac0
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-09 13:39:46 +0200

    test; eliminar import que no se usaba

--- berri_bank/src/test/java/edu/poniperro/invoker/TestInvoker.java
+++ berri_bank/src/test/java/edu/poniperro/invoker/TestInvoker.java
@@ -2,7 +2,6 @@ package edu.poniperro.invoker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
